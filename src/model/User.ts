dfc6a083ed2651ad1ce7ad3ee0bb39cf84bef3b0
NISCHAL RAI <nischal.c.r1313@gmail.com>
2026-09-19 19:46:59 +0545

    feat(auth): add user mongoose schema

---
+++ src/model/User.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'staff';
+  createdAt: Date;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'staff'], default: 'admin' },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
